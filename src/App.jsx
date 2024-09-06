6473beb8c2d78c1c5d56635ca744329672210b31
LivsDev <153335120+nicholas-sc-08@users.noreply.github.com>
2024-09-05 22:15:56 -0300

    inclusao do AuthProvider para fornecer o contexto de autenticação à aplicação inteira no app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import { AuthProvider } from './context/auth'; // Importando o contexto de auten
 
 function App() {
     return (
-      <AuthProvider>
-      <div className="App">
-          <h1>Cadastro de Usuários</h1>
-          <RegisterForm />
-      </div>
-  </AuthProvider>
-);
+        <AuthProvider>
+            <div className="App">
+                <h1>Cadastro de Usuários</h1>
+                <RegisterForm />
+            </div>
+        </AuthProvider>
+    );
 }
 
 export default App;
